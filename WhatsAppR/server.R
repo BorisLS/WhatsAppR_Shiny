bd2919559dbe540ce7b4c922f7288eaf93a2cbb1
BorisLS <boris.luetke.schelhowe@gmail.com>
2017-02-11 12:33:17 +0100

    Initial commit // Deploying on shinyapps.io

--- WhatsAppR/server.R
+++ WhatsAppR/server.R
@@ -7,9 +7,17 @@
 #    http://shiny.rstudio.com/
 #
 
+# Package Management
 library(shiny)
-library(WhatsAppR)
+library(readr)
+library(lubridate)
+library(stringr)
 library(dplyr)
+library(tidytext)
+
+#devtools::install_github("BorisLS/WhatsAppR")
+library(WhatsAppR)
+#
 
 # Define server logic required to draw a histogram
 # By default, the file size limit is 5MB. It can be changed by
@@ -39,15 +47,130 @@ function(input, output) {
     
     imp_chat <- import_chat()
     
-    return(dim(imp_chat))
+    return(paste0("Insgesamt besitzt der importierte Chatverlauf ", 
+                  dim(imp_chat)[1],
+                  " Zeilen."))
   })
   
-  output$wapp_author <- renderTable({
+  
+  ###### Tabset Teilnehmer
+  
+  output$intro_author <- renderText({
+    
+    chat_author <- import_chat()
+    count_author <- length(unique(chat_author$author))
+    
+    return(paste0("In dem Chatverlauf sind ", count_author, " Teilnehmer enthalten.")) 
+  })
+  
+  output$wapp_author_all <- renderTable({
     
     chat_author <- import_chat() %>%
                    wapp_stat_author()
+    
     return(chat_author)
   })
   
+  output$wapp_author_text <- renderTable({
+    
+    chat_author <- import_chat() %>%
+                   wapp_filter(type = "text") %>%
+                   wapp_stat_author()
+    
+    return(chat_author)
+  })
+  
+  output$wapp_author_media <- renderTable({
+    
+    chat_author <- import_chat() %>%
+                   wapp_filter(type = "media") %>%
+                   wapp_stat_author()
+    
+    return(chat_author)
+  })
+  
+  
+  output$wapp_author_firstpost <- renderTable({
+    
+    chat_author <- import_chat() %>%
+                   wapp_filter(type = "message") %>%
+                   wapp_stat_firstpost()
+    
+    return(chat_author)
+  })
+  
+  ###### Tabset Datum
+  
+  output$wapp_date_all <- renderTable({
+    
+    chat_date <- import_chat() %>%
+                 wapp_stat_date(n=5)
+    
+    chat_date$day <- as.character(chat_date$day)
+    
+    return(chat_date)
+  })
+  
+  
+  output$wapp_date_text <- renderTable({
+    
+    chat_date <- import_chat() %>%
+                 wapp_filter(type = "message") %>%
+                 wapp_stat_date(n=5)
+    
+    chat_date$day <- as.character(chat_date$day)
+    
+    return(chat_date)
+  })
+  
+  output$wapp_date_media <- renderTable({
+    
+    chat_date <- import_chat() %>%
+                 wapp_filter(type = "media") %>%
+                 wapp_stat_date(n=5)
+    
+    chat_date$day <- as.character(chat_date$day)
+    
+    return(chat_date)
+  })
+  
+  output$wapp_stat_time_all <- renderTable({
+    
+    chat_date <- import_chat() %>%
+                 wapp_stat_time(interval = "quarter")
+
+    return(chat_date)
+  })
+  
+  ###### Tabset Emoticon
+  output$wapp_emoticons_all <- renderTable({
+    
+    chat_emo <- import_chat()
+    
+    author <- unique(chat_emo$author)
+    
+    table_emo <- data.frame(author = "first line",
+                            char = "first line",
+                            description = "first line",
+                            number = 0,
+                            share = 0.01,
+                            stringsAsFactors = FALSE)
+    for(aut in author){
+      
+      df.aut <- chat_emo %>%
+        wapp_stat_emoticons(author = aut, 
+                            n = 3)
+      
+      df.aut$author <- aut
+      
+      table_emo <- bind_rows(table_emo, df.aut)
+      
+    }
+    
+    table_emo <- table_emo %>% filter(char != "first line")
+
+    return(table_emo)
+  })
+
   
 }
--- WhatsAppR/ui.R
+++ WhatsAppR/ui.R
@@ -10,10 +10,10 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
-  titlePanel("Uploading Files"),
+  titlePanel("Analyse von WhatsApp Chatverläufe mit WhatsAppR"),
   sidebarLayout(
     sidebarPanel(
-      fileInput('file1', 'Choose file to upload',
+      fileInput('file1', 'Wähle Chatverlauf aus',
                 accept = c(
                   'text/csv',
                   'text/comma-separated-values',
@@ -23,28 +23,65 @@ fluidPage(
                   '.tsv'
                 )
       ),
-      actionButton("start", "Analyze"),
-      p('Geduld.....'),
       tags$hr(),
       radioButtons('os', 'OS',
                    c(Android='android',
                      iOS='iOS'),
                    'android'),
       radioButtons('language', 'Language',
-                   c('German'='de',
-                     'English'='en'),
+                   c('Deutsch'='de'),
                    'de'),
       tags$hr(),
-      p('If you want a sample .csv or .tsv file to upload,',
-        'you can first download the sample',
-        a(href = 'mtcars.csv', 'mtcars.csv'), 'or',
-        a(href = 'pressure.tsv', 'pressure.tsv'),
-        'files, and then try uploading them.'
-      )
+      actionButton("start", "Analyse"),
+      tags$hr(),
+      p('Weitere Informationen zu dem WhatsAppR-Package',
+        'stehen unter',
+        a(href = 'https://github.com/BorisLS/WhatsAppR', 'https://github.com/BorisLS/WhatsAppR'),
+        'bereit.'
+      ),
+      tags$hr(),
+      p('Boris.Luetke.Schelhowe (at) gmail.com')
     ),
+    
     mainPanel(
-      verbatimTextOutput('laenge'),
-      tableOutput('wapp_author')
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Zusamenfassung",
+                           p("Diese Webapp ermöglicht es WhatsApp-Chats auf Basis des R-Packages WhatsAppR zu analysieren. Als Grundlage dient der als Textdatei exportierte Chatverlauf."),
+                           p("Eine Anleitung wie der Chatverlauf eines Chats als txt-Datei exportiert werden kann, findet sich für das jeweilige Betriebssystem in den FAQs von WhatsApp:",
+                             a(href = 'https://www.whatsapp.com/faq/de/android/23756533', 'Android'), "oder",
+                             a(href = 'https://www.whatsapp.com/faq/de/iphone/20888066#email', 'iOS'), "."
+                           ),
+                           p("Als Einstellung ist der Export ohne Medien zu wählen."),
+                           p("Der exportierte Chatverlauf ist zuerst auszuwählen und hochzuladen."),
+                           p("Im Anschluss ist auszuwählen, welche Sprache die Systemeinstellung des Smartphones ist und welches Betriebssystem das Smartphone hat."),
+                           p("Auf den verschiedenen Tabs finden sich die jeweiligen Auswertungen, die nach Betätigen von Analyse erstellt wurden."),
+                           verbatimTextOutput('laenge')), 
+                  tabPanel("Teilnehmer",
+                           textOutput("intro_author"),
+                           p("Die nachfolgenden Tabelle stellt dar, wie sich die versendeten Nachrichten auf die einzelnen Teilnehmer aufteilen:"),
+                           tableOutput("wapp_author_all"),
+                           p("Die nachfolgenden Tabelle dagegen umfasst nur die die versendeten Textnachrichten:"),
+                           tableOutput("wapp_author_text"),
+                           p("Die nachfolgenden Tabelle dagegen umfasst nur die die versendeten Nachrichten, die keine Textnachrichten sind (z.B. Bilder, Videos):"),
+                           tableOutput("wapp_author_media"),
+                           p("In Gruppenchat existieren einzelne Personen die meist den ersten Post des Tages durchführen und damit den Chatverlauf des Tages initiieren. Die nachfolgende Tabelle stellt dar, wie häufig ein Teilnehmer den ersten Post des Tages erstellt hat."),
+                           tableOutput("wapp_author_firstpost")
+                           ),
+                  tabPanel("Datum",
+                           p("Die nachfolgenden Tabelle stellt dar, an welchen 5 Tagen die meisten Nachrichten versendet wurden:"),
+                           tableOutput("wapp_date_all"),
+                           p("Die nachfolgenden Tabelle dagegen umfasst nur die die versendeten Textnachrichten:"),
+                           tableOutput("wapp_date_text"),
+                           p("Die nachfolgenden Tabelle dagegen umfasst nur die die versendeten Nachrichten, die keine Textnachrichten sind (z.B. Bilder, Videos):"),
+                           tableOutput("wapp_date_media"),
+                           p("Die stellt dar in welchen Zeiträumen am Tag die Nachrichten in der Gruppe versendet wurden:"),
+                           tableOutput("wapp_stat_time_all")
+                          ),
+                  tabPanel("Emoticon",
+                           p("Emoticons sind ein wichtiger Bestandteil in der Kommunikation mit WhatsApp. Die nachfolgende Tabelle stellt dar, welche 3 Emoticons jeder Teilnahmer in dem Chat am häufigsten verwendet."),
+                           tableOutput("wapp_emoticons_all")
+                  )
+              )
     )
   )
 )
